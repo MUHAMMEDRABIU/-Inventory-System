2b1d531cb92c94508e7b80d0ab52e9126bf5278c
ammarr2211 <ammarladan01@gmail.com>
2025-05-28 20:02:49 +0100

    i updated the deleteitem.php under item in model folder

--- model/item/deleteItem.php
+++ model/item/deleteItem.php
@@ -1,42 +1,43 @@
 <?php
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
-	
-	$itemNumber = htmlentities($_POST['itemDetailsItemNumber']);
-	
-	if(isset($_POST['itemDetailsItemNumber'])){
-		
-		// Check if mandatory fields are not empty
-		if(!empty($itemNumber)){
-			
-			// Sanitize item number
-			$itemNumber = filter_var($itemNumber, FILTER_SANITIZE_STRING);
+require_once('../../inc/config/constants.php');
+require_once('../../inc/config/db.php');
 
-			// Check if the item is in the database
-			$itemSql = 'SELECT itemNumber FROM item WHERE itemNumber=:itemNumber';
-			$itemStatement = $conn->prepare($itemSql);
-			$itemStatement->execute(['itemNumber' => $itemNumber]);
-			
-			if($itemStatement->rowCount() > 0){
-				
-				// Item exists in DB. Hence start the DELETE process
-				$deleteItemSql = 'DELETE FROM item WHERE itemNumber=:itemNumber';
-				$deleteItemStatement = $conn->prepare($deleteItemSql);
-				$deleteItemStatement->execute(['itemNumber' => $itemNumber]);
+// Check if item number was sent via POST
+if (!isset($_POST['itemDetailsItemNumber'])) {
+    echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item number not provided.</div>';
+    exit();
+}
 
-				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Item deleted.</div>';
-				exit();
-				
-			} else {
-				// Item does not exist, therefore, tell the user that he can't delete that item 
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item does not exist in DB. Therefore, can\'t delete.</div>';
-				exit();
-			}
-			
-		} else {
-			// Item number is empty. Therefore, display the error message
-			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the item number</div>';
-			exit();
-		}
-	}
+// Sanitize and validate input
+$itemNumber = trim(htmlentities($_POST['itemDetailsItemNumber']));
+$itemNumber = filter_var($itemNumber, FILTER_SANITIZE_STRING);
+
+if (empty($itemNumber)) {
+    echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the item number.</div>';
+    exit();
+}
+
+try {
+    // Check if item exists in the database
+    $checkSql = 'SELECT itemNumber FROM item WHERE itemNumber = :itemNumber';
+    $checkStmt = $conn->prepare($checkSql);
+    $checkStmt->execute(['itemNumber' => $itemNumber]);
+
+    if ($checkStmt->rowCount() === 0) {
+        echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item does not exist in the database. Cannot delete.</div>';
+        exit();
+    }
+
+    // Proceed to delete item
+    $deleteSql = 'DELETE FROM item WHERE itemNumber = :itemNumber';
+    $deleteStmt = $conn->prepare($deleteSql);
+    $deleteStmt->execute(['itemNumber' => $itemNumber]);
+
+    echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Item successfully deleted.</div>';
+    exit();
+} catch (PDOException $e) {
+    // Log error in production
+    echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Error deleting item: ' . htmlspecialchars($e->getMessage()) . '</div>';
+    exit();
+}
 ?>
